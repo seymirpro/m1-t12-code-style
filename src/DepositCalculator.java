f4964a688f12f863b2fa49504e4e5b25d9eb517b
seymirpro <seymirpro@yandex.ru>
2022-10-02 12:15:23 +0300

    code style fix

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -12,7 +12,6 @@ public class DepositCalculator {
 
     double roundUpperBound(double value, int places) {
         double scale = Math.pow(10, places);
-
         return Math.round(value * scale) / scale;
     }
 
